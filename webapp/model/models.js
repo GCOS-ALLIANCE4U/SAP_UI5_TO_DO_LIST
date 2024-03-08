e29fdde4baf6013fbd1550b0eea83ca346468a97
Gulcan COSKUN <gulcan.coskun@alliance4u.fr>
2024-03-08 14:42:35 +0100

    Create an object todo_list in the model

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -19,10 +19,58 @@ sap.ui.define([
                 oModel.setDefaultBindingMode("OneWay");
                 return oModel;
         },
-
+// Create object todo_list
         createTodoModel : function() {
             var oModel = new JSONModel({
-                todo_list : []
+    
+                todo_list : [{
+                    title: "Scrum Master",
+                    description: "Passing the Professional Scrum Master Exam",
+                    targetDate: new Date("02/21/2024"),
+                    status : 0
+                },
+                {
+                    title: "Scrum Developer",
+                    description: "Passing the Professional Scrum Developer Exam",
+                    targetDate: new Date("03/12/2024"),
+                    status : 0
+                }
+                ,
+                {
+                    title: "PRINCE2",
+                    description: "Passing the exam PRINCE2",
+                    targetDate: new Date("04/21/2024"),
+                    status : 0
+                }
+                ,
+                {
+                    title: "SAPUI5",
+                    description: "Creating a to do list app",
+                    targetDate: new Date("04/30/2024"),
+                    status : 0
+                }
+                ,
+                {
+                    title: "SAPUI5",
+                    description: "Create a SAPUI5 App",
+                    targetDate: new Date("05/12/2024"),
+                    status : 0
+                }
+                ,
+                {
+                    title: "GITHUB",
+                    description: "Create a second GitHub Account",
+                    targetDate: new Date("06/21/2024"),
+                    status : 0
+                }
+                ,
+                {
+                    title: "French",
+                    description: "Develop your French language skills",
+                    targetDate: new Date("06/05/2024"),
+                    status : 0
+                }
+            ]
             });
             return oModel;
         }
