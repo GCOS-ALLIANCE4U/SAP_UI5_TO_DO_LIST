32bc3665e9b1253cfb372ae9ce4b1252d306a168
Gulcan COSKUN <gulcan.coskun@alliance4u.fr>
2024-03-08 17:14:42 +0100

    createTodo function to open a fragment

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "gcosui5todolist/utils/formatter"
+    "gcosui5todolist/utils/formatter",
+    "sap/ui/core/Fragment"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, formatter) {
+    function (Controller, formatter, Fragment) {
         "use strict";
 
         return Controller.extend("gcosui5todolist.controller.Main", {
@@ -23,8 +25,23 @@ sap.ui.define([
                 oModel.setProperty("status", "1", oContext);
         
             },
+            // if createDialog is not present, create a fragment
             createTodo: function() {
-                
+                if(!this.createDialog){
+                    this.createDialog = Fragment.load({
+                        type:"XML",
+                        name:"gcosui5todolist.fragment.create"
+                    }).then(function(oDialog) {
+
+                        this.getView().addDependent(oDialog);
+                        return oDialog;
+                    }.bind(this))
+
+                }
+                this.createDialog.then(function(oDialog) {
+                    oDialog.open();
+
+                })
             }
         });
     });
