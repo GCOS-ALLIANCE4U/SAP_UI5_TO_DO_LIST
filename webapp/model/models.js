fee23786cdaabf190a5d0935f8c07ec1f89d5b90
Gulcan COSKUN <gulcan.coskun@alliance4u.fr>
2024-03-08 14:57:30 +0100

    use items keyword to bind list items

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/Device"
-], 
+],
     /**
      * provide app-view type models (as in the first "V" in MVVC)
      * 
@@ -18,61 +18,62 @@ sap.ui.define([
                 var oModel = new JSONModel(Device);
                 oModel.setDefaultBindingMode("OneWay");
                 return oModel;
-        },
-// Create object todo_list
-        createTodoModel : function() {
-            var oModel = new JSONModel({
-    
-                todo_list : [{
-                    title: "Scrum Master",
-                    description: "Passing the Professional Scrum Master Exam",
-                    targetDate: new Date("02/21/2024"),
-                    status : 0
-                },
-                {
-                    title: "Scrum Developer",
-                    description: "Passing the Professional Scrum Developer Exam",
-                    targetDate: new Date("03/12/2024"),
-                    status : 0
-                }
-                ,
-                {
-                    title: "PRINCE2",
-                    description: "Passing the exam PRINCE2",
-                    targetDate: new Date("04/21/2024"),
-                    status : 0
-                }
-                ,
-                {
-                    title: "SAPUI5",
-                    description: "Creating a to do list app",
-                    targetDate: new Date("04/30/2024"),
-                    status : 0
-                }
-                ,
-                {
-                    title: "SAPUI5",
-                    description: "Create a SAPUI5 App",
-                    targetDate: new Date("05/12/2024"),
-                    status : 0
-                }
-                ,
-                {
-                    title: "GITHUB",
-                    description: "Create a second GitHub Account",
-                    targetDate: new Date("06/21/2024"),
-                    status : 0
-                }
-                ,
-                {
-                    title: "French",
-                    description: "Develop your French language skills",
-                    targetDate: new Date("06/05/2024"),
-                    status : 0
-                }
-            ]
-            });
-            return oModel;
-        }
-    };
-});
+            },
+            // Create object todo_list
+            // in the component file, I set the mTodo model
+            createTodoModel: function () {
+                var oModel = new JSONModel({
+
+                    todo_list: [{
+                        title: "Scrum Master",
+                        description: "Passing the Professional Scrum Master Exam",
+                        targetDate: new Date("02/21/2024"),
+                        status: 0
+                    },
+                    {
+                        title: "Scrum Developer",
+                        description: "Passing the Professional Scrum Developer Exam",
+                        targetDate: new Date("03/12/2024"),
+                        status: 0
+                    }
+                        ,
+                    {
+                        title: "PRINCE2",
+                        description: "Passing the exam PRINCE2",
+                        targetDate: new Date("04/21/2024"),
+                        status: 0
+                    }
+                        ,
+                    {
+                        title: "SAPUI5",
+                        description: "Creating a to do list app",
+                        targetDate: new Date("04/30/2024"),
+                        status: 0
+                    }
+                        ,
+                    {
+                        title: "SAPUI5",
+                        description: "Create a SAPUI5 App",
+                        targetDate: new Date("05/12/2024"),
+                        status: 0
+                    }
+                        ,
+                    {
+                        title: "GITHUB",
+                        description: "Create a second GitHub Account",
+                        targetDate: new Date("06/21/2024"),
+                        status: 0
+                    }
+                        ,
+                    {
+                        title: "French",
+                        description: "Develop your French language skills",
+                        targetDate: new Date("06/05/2024"),
+                        status: 0
+                    }
+                    ]
+                });
+                return oModel;
+            }
+        };
+    });
